6270cf13dc502d519ee2d007af042b6c2db539a7
AHMED <ahmedaltahir77@gmail.com>
2023-09-07 12:34:58 -0700

    zoooooouooo6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,17 @@
 
 /**
  * main- Entry point
- * 
- * Description: print a qoute using write function
- *     ssize_t write(int fd, const void *buf, size_t count);
  *
- * Return: 1 (faliuer).
+ * Description: print a qoute using write function
+ * ssize_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: 1 (failuer).
 */
 
 int main(void)
 {
 char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 write(1, quo, 59);
 return (1);
 }
