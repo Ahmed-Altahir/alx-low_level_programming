0e53ea6323fa12f64e42499e85674342cb9ce5c1
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-10 22:29:37 +0200

    a7aooaa

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main entry point
+ * 
+ * Description: alphabet in lowercase
+ *
+ * Return: 0 (successful)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <='z')
+	{
+		putchar(ch)
+		ch++;
+	}
+	putchar("\n")
+
+return (0);
+}
