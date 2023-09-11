bcff6fc02ccbecf49f450b82eb8df20f91050d3c
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-11 13:58:47 +0200

    a7eeyy

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
-/**                                                                                                                                         
- * main - entry point                                                                                                                       
- *                                                                                                                                          
- * Description: print digit numbers from 0 to 9                                                                                             
- *                                                                                                                                          
- * Return: 0 (successful)                                           
+/**
+
+ * main - entry point
+
+ *
+
+ * Description: print digit numbers from 0 to 9
+
+ *
+
+ * Return: 0 (successful)
 */
 
 int main(void)
