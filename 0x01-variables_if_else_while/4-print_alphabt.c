3d845506824188adf151aa9ddb8a8413971d7219
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-11 13:38:54 +0200

    a7eeedddc

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,13 @@
  * Return: 0 (successful)
 */
 
-int main()
+int main(void)
 {
 	char ch = 'a';
 
-        while (ch <= 'z')
+	while (ch <= 'z')
 	{
-		if (ch == 'e' || ch =='q')
+		if (ch == 'e' || ch == 'q')
 			ch++;
 		putchar(ch);
 		ch++;
