83ddf945a60e448c14259eb7b014a6ba82b20cfc
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-13 14:47:19 +0200

    a7eeeeheee

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - function to check if characyer is lowercase
+ *
+ * print_alphabet - use _putchar function to print_alphabet
+ *
+ * @n: takes integer type input for function
+ *
+ * Return: returns 1 if c is lowercase otherwise always 0 (successful)
+*/
+
+int _abs(int
+{		
+		if (n < 0)
+		n = (-1) * n;
+		return (n);
+}
