2523f93ce4b6c7c184b09b442da69706fc3c1af2
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-15 18:31:33 +0200

    a7eeeeh

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Description: mul - check if c is upper
+ *
+ * Return: always 0 
+*/
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <=9);
+	_putchar('\n');
+}
