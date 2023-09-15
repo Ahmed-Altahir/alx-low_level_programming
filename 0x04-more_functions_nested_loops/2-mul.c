115ccbc11a93b4bd2119227205c18146cf32a370
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-15 18:25:58 +0200

    a7eeeeh

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Description: mul - check if c is upper
+ *
+ * @a: input for alphabet
+ * @b: input for alphabet
+ *
+ * Return: 1 if its uppper, 0 if not
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
