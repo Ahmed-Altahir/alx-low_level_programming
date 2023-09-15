47a8dd5647cfee13ad7a235a46a6559c5f1b9ab6
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-15 19:09:03 +0200

    a7eeeeh

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,15 +5,15 @@
  *
  * @n: num of the _ times
  *
- * Return: always 0
+ *
 */
 
 void print_line(int n)
 {
 	int shit;
+
 	if (n <= 0)
 		_putchar('\n');
-	
 	else
 	{
 		for (shit = 1; shit <= n; shit++)
