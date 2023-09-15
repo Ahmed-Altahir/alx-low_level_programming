1bfd08ed27011086a30ebd5a7c90c2f5e1184cf8
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-15 20:14:36 +0200

    a7eeeeh

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,15 +12,15 @@ void print_triangle(int size)
 {
 	int h, b;
 
-	if (s <= 0)
+	if (size <= 0)
 		_putchar('\n');
 	else
 	{
-		for (h = 1; h <= s; h++)
+		for (h = 1; h <= size; h++)
 		{
-			for (b = 1; b <= s; b++)
+			for (b = 1; b <= size; b++)
 			{
-				if ((h + b) <= s)
+				if ((h + b) <= size)
 					_putchar(' ');
 				else
 					_putchar('#');
