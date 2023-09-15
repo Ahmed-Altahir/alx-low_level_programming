6e3aed05c582c9912441442d750996d16269765d
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-15 19:03:20 +0200

    a7eeeeh

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - check if c is upper
+ *
+ * @n: ugkh khk hkb
+ *
+ * Return: always 0
+*/
+
+void print_line(int n)
+{
+	int shit;
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (shit = 1; shit <= n; shit++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
