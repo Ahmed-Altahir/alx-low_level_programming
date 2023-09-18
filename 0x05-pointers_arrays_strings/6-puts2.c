3a837555d79374462ff268936424a4d74a46ab81
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-18 20:48:43 +0200

    gi

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _puts2 - check the code
+ *
+ * @str: int
+ *
+ * Return: nothing.
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int p = 0;
+
+	while (str(p) != '\0')
+	{
+		p++;
+	}
+
+	for (i = 0; i < p; i+= 2)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
