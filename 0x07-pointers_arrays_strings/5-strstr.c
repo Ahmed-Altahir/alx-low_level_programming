aed8233da1bb44bfe21e47402194b6722f3dc6a4
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-25 22:53:26 +0200

    a7eeeeh

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strstr - check the code
+ *
+ * @haystack: string
+ * @needle: int
+ *
+ *
+ * Return: void
+ */
+
+char *_strstr(char *haystack, char *needle)
+
+{
+
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - check the code
+ *
+ * @a: string
+ * @c: int
+ *
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+
+{
+	int i, o;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (o = 0; 0 < 8; o++)
+		{
+			_putchar(a[i][o]);
+		}
+		_putchar('\n');
+	}
+
+}
