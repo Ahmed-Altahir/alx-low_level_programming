4c23abbc5e5183a34f61f317965f8828121deb7d
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-25 23:13:48 +0200

    a7eeeeh

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - check the code
+ *
+ * @a: string
+ * @size: blablabla
+ *
+ * Return: void
+*/
+
+void print_diagsums(int *a, int size)
+
+{
+	int i, e1 = 0, e2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		e1 += a[i];
+		e2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d, ", e1);
+	printf("%d\n, ", e2);
+
+}
