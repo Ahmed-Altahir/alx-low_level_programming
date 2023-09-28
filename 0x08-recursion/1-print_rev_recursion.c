5a0196db4677c9dbeabb002475f58c20f328d5d7
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-28 21:08:56 +0200

    a7eeeeh

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  _print_rev_recursion - check the code
+ *
+ * @s: string
+ *
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+
+}
