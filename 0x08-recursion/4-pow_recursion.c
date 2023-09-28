acb34f60097882ced4042ac20c54839c143145d2
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-28 21:31:22 +0200

    a7eeeeh

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - check the code
+ *
+ * @y: string
+ * @x: jdgvcjs
+ *
+ *
+ * Return: void
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	else if (y == 0)
+		return (1);
+
+	return  (x * _pow_recursion(x, y -1));
+}
