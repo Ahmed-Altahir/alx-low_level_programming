2b8ac0e31e49de7bd39ef64c62c6cc467abd992b
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-28 21:16:07 +0200

    a7eeeeh

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - ck the code
+ *
+ * @s: string
+ *
+ * Return: void
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*S > '\0')
+	{
+		n += _strlen_recursion(s + 1) + 1;
+	}
+
+	return(n);
+}
