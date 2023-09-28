a3febfe1c1e0aa7f7ce20d0d5cc42ea31afb14a3
AHMEDbash <ahmedaltahir77@gmail.com>
2023-09-28 21:04:03 +0200

    a7eeeeh

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -14,11 +14,11 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 	else
 	{
-		_putchar(*s);
-		_puts_recursion(s +1);
+		putchar(*s);
+		_puts_recursion(s + 1);
 	}
 }
