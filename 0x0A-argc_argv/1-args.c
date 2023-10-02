b87164600d91f04dc53b8423e5e285434e771b53
AHMEDbash <ahmedaltahir77@gmail.com>
2023-10-03 00:46:47 +0200

    lmjnkj

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * @argc: string
+ * @argv: blabla
+ *
+ *
+ * Return: void
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
