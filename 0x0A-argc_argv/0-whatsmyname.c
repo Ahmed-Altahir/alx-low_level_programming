10c9ecbf53092b158f4a12abb9dfc3f5caf0491c
AHMEDbash <ahmedaltahir77@gmail.com>
2023-10-03 00:39:12 +0200

    lmjnkj

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * @argc: string
+ * @argv: blabla
+ *
+ *
+ * Return: void
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
