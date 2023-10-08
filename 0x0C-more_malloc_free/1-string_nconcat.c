500938479e8276a67fea45d4069ebbff2f91257d
AHMEDbash <ahmedaltahir77@gmail.com>
2023-10-08 22:46:46 +0200

    fdsdf

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,6 @@
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-
 {
 	char *str;
 	unsigned int i, j, s1_length, s2_length;
@@ -24,9 +23,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
 		;
+
 	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
                 ;
 
+
+	
 	str = malloc(s1_length + n + 1);
 	if (str == NULL)
 	{
