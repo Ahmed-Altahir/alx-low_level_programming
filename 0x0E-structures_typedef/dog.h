7dd5d65d0104c9cf488890fc1730d6fb4940b837
AHMEDbash <ahmedaltahir77@gmail.com>
2023-10-10 22:15:10 +0200

    fdsdf

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _dog_H
+#define _dog_H
+
+/**
+ * struct dog - wdwd
+ * @name: hdcuecnu
+ * @age: hjdchw
+ * @owner: jdwkdj
+ *
+ * Description: wceuineiui
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
