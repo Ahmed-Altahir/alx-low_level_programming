6f964f960a6d42fd6ad4edffd7bf679b838fa8d9
AHMEDbash <ahmedaltahir77@gmail.com>
2023-10-10 23:36:48 +0200

    fdsdf

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - wdwd
+ * @name: hdcuecnu
+ * @age: hjdchw
+ * @owner: jdwkdj
+ * @d: ndcne
+ *
+ * Return: wceuineiui
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+        if (d)
+        {
+                d->name = name;
+                d->age = age;
+                d->owner = owner;
+        }
