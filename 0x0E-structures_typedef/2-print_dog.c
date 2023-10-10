e5c06aec666b089cfe8c17f2d7eaa9ffc0038dc3
AHMEDbash <ahmedaltahir77@gmail.com>
2023-10-10 23:24:42 +0200

    fdsdf

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - wdwd
+ * @d: ndcne
+ *
+ * Return: wceuineiui
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner : "(nil)");
+	}
+}
